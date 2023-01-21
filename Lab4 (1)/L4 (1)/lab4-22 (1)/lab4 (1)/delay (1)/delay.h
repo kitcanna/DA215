ecf83e3e9711d3b13c012a3ee94fc058e9b3bc4d
Anna Selstam <89905869+kitcanna@users.noreply.github.com>
2023-01-21 17:13:43 +0100

    L4

---
+++ Lab4 (1)/L4 (1)/lab4-22 (1)/lab4 (1)/delay (1)/delay.h
@@ -0,0 +1,24 @@
+﻿/*
+ * delay.h
+ * 
+ * These delay functions are external assembly subroutines.
+ * See 'delay_asm.s' for details.
+ *
+ * Author:	Mathias Beckius
+ * Date:	2014-11-29
+ * Editor:	Malin Ramkull & Anna Selstam
+ * Date:	2021-12-09
+ *
+ */
+
+#ifndef DELAY_H_
+#define DELAY_H_
+
+#include <inttypes.h>
+
+extern void delay_1_micros(void);
+extern void delay_micros(uint8_t);
+extern void delay_ms(uint8_t);
+extern void delay_s(uint8_t);
+
+#endif /* DELAY_H_ */
